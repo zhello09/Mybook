bad08da1091452a33a053bdba193fcbef23bd339
zhello09 <65401822+zhello09@users.noreply.github.com>
2021-06-23 17:12:43 +0900

    Add Activity Layout

---
+++ app/src/main/java/com/example/mybook/home_activity.java
@@ -0,0 +1,14 @@
+package com.example.mybook;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class home_activity {
+    Intent intent;
+
+
+}
---
+++ app/src/main/java/com/example/mybook/login_activity.java
@@ -0,0 +1,4 @@
+package com.example.mybook;
+
+public class login_activity {
+}
